65c91805211c8ea0082183041ee9658febe48dd2
Nishadi rajapaksha <93418323+Nish-rajapaksha@users.noreply.github.com>
2022-07-04 23:56:33 +0530

    DashBoard Create

--- src/controllers/DashBoardFormController.java
+++ src/controllers/DashBoardFormController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+
 public class DashBoardFormController {
+    public ComboBox cmbTemType1;
+    public ComboBox cmbTemType2;
+    public JFXTextField txtTem;
+    public JFXTextField txtTem2;
+    public void initialize () {
+        cmbTemType1.getItems().add("Celcius");
+        cmbTemType1.getItems().add("Farenhite");
+        cmbTemType1.getItems().add("Kelvin");
+
+
+        cmbTemType2.getItems().add("Celcius");
+        cmbTemType2.getItems().add("Farenhite");
+        cmbTemType2.getItems().add("Kelvin");
+
+    }
+    public void btnTemConvertOnAction(ActionEvent actionEvent) {
+
+    }
 }
